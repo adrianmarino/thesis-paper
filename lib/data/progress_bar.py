c87aca709419419ce86a701cfaa018c659401f5e
adrianmarino <adrianmarino@gmail.com>
2022-05-18 18:18:55 -0300

    fix: select tqdm imoplementation (ipython or python)

--- lib/data/progress_bar.py
+++ lib/data/progress_bar.py
@@ -1,4 +1,14 @@
-from tqdm.notebook import tqdm_notebook
+def run_from_ipython():
+    try:
+        __IPYTHON__
+        return True
+    except NameError:
+        return False
+
+if run_from_ipython():
+    from tqdm.notebook import tqdm_notebook as tqdm
+else:
+    from tqdm import tqdm
 
 def progress_bar(count, title='Processing'): 
-    return tqdm_notebook(total=count, desc = title)
+    return tqdm(total=count, desc = title)
