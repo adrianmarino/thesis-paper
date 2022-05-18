5cffeed9671383a653d4911865bf3a6e93754024
adrianmarino <adrianmarino@gmail.com>
2022-05-18 17:39:24 -0300

    fix: seqeunce test

--- tests/test_sequence.py
+++ tests/test_sequence.py
@@ -7,26 +7,15 @@ import pandas as pd
 import numpy as np
 
 
-def check_sequence(values):
-    last = -1
-    for seq_value in values:
-        if last == -1:
-            last = seq_value
-            continue
-
-        assert last == seq_value-1
-        last = seq_value
-
-
 class TestSequence:
     def test_consecurive_sequence(self):
         # Prepare...
         df  = pd.DataFrame({ 'id': list(range(1, 1001, 5)) })
-        seq = dt.Sequencer()
+        sec = dt.Sequencer('id', 'seq')
 
         # Perform
-        df['seq'] = df.id.apply(seq.get)
+        df = sec.perform(df)
 
         # Assert
         sequence = np.unique(df['seq'].values)
-        check_sequence(sequence)
+        dt.check_sequence(sequence)
