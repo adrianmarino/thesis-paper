dcf080bfc2c6dea1e1e37a2dd77ca5f7202fc2e0
adrianmarino <adrianmarino@gmail.com>
2022-05-24 12:02:10 -0300

    feat: adjust deep fm model

--- lib/model/validate/validator.py
+++ lib/model/validate/validator.py
@@ -6,16 +6,6 @@ import torch.multiprocessing as mp
 import os
 
 
-def evaluate_fn(p, y_true, X, sample, metrics, kwargs):
-    y_pred = p.predict_batch(X, **kwargs)
-
-    result = { 'predictor': p.name, 'sample': sample}
-    for m in metrics:
-        result.update(m.perform(y_pred, y_true, X))
-
-    return result
-
-
 class Validator:
     def __init__(self, n_samples, batch_size, metrics, predictors, n_processes=5, target_transform_fn = lambda it: it.squeeze(1)):
         self.n_samples   = n_samples
@@ -25,8 +15,18 @@ class Validator:
         self.n_processes = n_processes
         self.target_transform_fn = target_transform_fn
 
-    def validate(self, ds,  **kwargs):
-        os.environ['MKL_THREADING_LAYER'] = 'GNU'
+    def __evaluate(self, y_true, X, sample, kwargs):
+        summary = []
+        for p in self.predictors:
+            y_pred = p.predict_batch(X, **kwargs)
+            result = { 'predictor': p.name, 'sample': sample}
+            for m in self.metrics:
+                result.update(m.perform(y_pred, y_true, X))
+                summary.append(result)
+        return summary
+
+
+    def validate(self, ds, **kwargs):
         summary = []
         with dt.progress_bar(self.n_samples) as bar:
             for sample in range(self.n_samples):
@@ -34,9 +34,7 @@ class Validator:
                 y_true    = self.target_transform_fn(y_true)
 
                 for p in self.predictors:
-                    summary.append(evaluate_fn(p, y_true, X, sample, self.metrics, kwargs))
-
+                    summary.extend(self.__evaluate(y_true, X, sample, kwargs))
                 bar.update()
-
 
         return ValidatorSummary.from_list(summary)
