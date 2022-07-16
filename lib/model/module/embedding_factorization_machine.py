aad8bc2a40372291e6535564fd4d8de53fa30c0d
adrianmarino <adrianmarino@gmail.com>
2022-07-16 19:14:39 -0300

    feat: methods section completed! ;) :)

--- lib/model/module/embedding_factorization_machine.py
+++ lib/model/module/embedding_factorization_machine.py
@@ -6,5 +6,6 @@ from torch.nn import Module
 class EmbeddingFactorizationMachine(Module, CommonMixin):
     def forward(self, x):
         square_of_sum = sum(x, dim=1) ** 2
-        sum_of_squares = sum(x ** 2, dim=1)
+        sum_of_squares = sum(x ** 2, dim=1) 
         return 0.5 * sum((square_of_sum - sum_of_squares), dim=1, keepdim=True)
+
