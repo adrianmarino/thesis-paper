ad75dca7e5958a4f683f1b063dbb0ca2de39cb7f
adrianmarino <adrianmarino@gmail.com>
2024-06-25 13:00:50 -0300

    feat: llama3 ensemble evaluation reached 50 sessions with at least 30 rated items. Also add plot to see tared items dist for these cases

--- chat-bot-api/models/evaluation/sessions_plotter.py
+++ chat-bot-api/models/evaluation/sessions_plotter.py
@@ -20,7 +20,7 @@ class SessionsPlotter:
     def plot_mean_ndcg_by_session_step_dist(self, figsize=(20, 6)):
         dpl.describe_num_var_array(
             self.sessions.ndcg,
-            "Mean NDCG by Session Step Distribution",
+            'Mean NDCG by Session Step Distribution',
             figsize=figsize,
         )
 
@@ -31,9 +31,9 @@ class SessionsPlotter:
             self.sessions.steps_by_index.mean_ndcg(
                 min_sessions_by_step=min_sessions_by_step
             ),
-            xlabel="Session Step",
-            ylabel="Mean NDGC",
-            title=f"Mean NDGC by Session Step (Min sessions by step = {min_sessions_by_step})",
+            xlabel='Session Step',
+            ylabel='Mean NDGC',
+            title=f'Mean NDGC by Session Step (Min sessions by step = {min_sessions_by_step})',
             smooth_level=1,
             figsize=figsize,
         )
@@ -45,9 +45,9 @@ class SessionsPlotter:
     ):
         dpl.plot_smooth_line(
             self.sessions.steps_by_index.mean_average_precision(min_sessions_by_step),
-            xlabel="Session Step",
-            ylabel="Mean Average Precision",
-            title=f"Mean Average Precision by Session Step Series (Min sessions by step = {min_sessions_by_step})",
+            xlabel='Session Step',
+            ylabel='Mean Average Precision',
+            title=f'Mean Average Precision by Session Step Series (Min sessions by step = {min_sessions_by_step})',
             smooth_level=1,
             figsize=figsize,
         )
@@ -59,9 +59,9 @@ class SessionsPlotter:
     ):
         dpl.plot_smooth_line(
             self.sessions.steps_by_index.mean_reciprocal_rank(min_sessions_by_step),
-            xlabel="Session Step",
-            ylabel="Mean Reciprocal Rank",
-            title=f"Mean Reciprocal Rank by Session Step Series (Min sessions by step = {min_sessions_by_step})",
+            xlabel='Session Step',
+            ylabel='Mean Reciprocal Rank',
+            title=f'Mean Reciprocal Rank by Session Step Series (Min sessions by step = {min_sessions_by_step})',
             smooth_level=1,
             figsize=figsize,
         )
@@ -73,56 +73,60 @@ class SessionsPlotter:
     ):
         dpl.plot_smooth_line(
             self.sessions.steps_by_index.mean_recall(min_sessions_by_step),
-            xlabel="Session Step",
-            ylabel="Mean Recall",
-            title=f"Mean Recall by Session Step Series (Min sessions by step = {min_sessions_by_step})",
+            xlabel='Session Step',
+            ylabel='Mean Recall',
+            title=f'Mean Recall by Session Step Series (Min sessions by step = {min_sessions_by_step})',
             smooth_level=1,
             figsize=figsize,
         )
 
     def metrics(self, item_ids=[]):
-        logging.info(f"Sessions: {self.count}")
+        logging.info(f'Sessions: {self.count}')
         logging.info(
-            f"Max Session Steps Reached: {len(self.sessions.steps_by_index.items)}"
+            f'Max Session Steps Reached: {len(self.sessions.steps_by_index.items)}'
         )
 
         logging.info(
-            f"Sessions count >= 30 rated items: {self.sessions.n_session_with_more_than(n_items=30)}"
+            f'Sessions count >= 30 rated items: {self.sessions.n_session_with_more_than(n_items=30)}'
         )
 
-        logging.info(f"Total Session Steps: {self.total_steps_count}")
+        logging.info(f'Total Session Steps: {self.total_steps_count}')
 
-        logging.info(f"Mean NDCG: {self.sessions.mean_ndcg:.2}")
+        logging.info(f'Mean NDCG: {self.sessions.mean_ndcg:.2}')
         logging.info(
-            f"Mean Average Precision: {self.sessions.mean_mean_average_precision:.2}"
+            f'Mean Average Precision: {self.sessions.mean_mean_average_precision:.2}'
         )
         logging.info(
-            f"Mean Reciprocal Rank: {self.sessions.mean_mean_reciprocal_rank:.2}"
+            f'Mean Reciprocal Rank: {self.sessions.mean_mean_reciprocal_rank:.2}'
         )
-        logging.info(f"Mean Recall: {self.sessions.mean_recall:.2}")
+        logging.info(f'Mean Recall: {self.sessions.mean_recall:.2}')
         if len(item_ids) > 0:
             logging.info(
-                f"Catalog Coverage: {self.sessions.catalog_coverage(item_ids):.2}"
+                f'Catalog Coverage: {self.sessions.catalog_coverage(item_ids):.2}'
             )
 
     def plot_n_steps_by_session_dist(self, figsize=(20, 6)):
         dpl.describe_num_var_array(
             [len(s) for s in self.sessions],
-            "Steps Count by Session Distribution",
+            'Steps Count by Session Distribution',
             figsize=figsize,
         )
 
     def plot_n_found_relevant_items_by_session_step_dist(self, figsize=(20, 6)):
         dpl.describe_num_var_array(
             self.sessions.n_found_relevant_items,
-            "Found items Count by Session Step Distribution",
+            'Found items Count by Session Step Distribution',
             figsize=figsize,
         )
 
-    def plot_n_rated_items_by_session_dist(self, figsize=(20, 6)):
+    def plot_n_rated_items_by_session_dist(
+        self,
+        figsize=(20, 6),
+        min_found_relevant_items = 0
+    ):
         dpl.describe_num_var_array(
-            [len(s.found_relevant_items) for s in self.sessions],
-            "Rated items by Session Distribution",
+            [len(s.found_relevant_items) for s in self.sessions if len(s.found_relevant_items) >= min_found_relevant_items],
+            f'Rated items by Session Distribution (min rated items: {min_found_relevant_items})',
             figsize=figsize,
         )
 
@@ -132,30 +136,30 @@ class SessionsPlotter:
                 (n_steps, len(steps))
                 for n_steps, steps in self.sessions.steps_by_index.items
             ],
-            xlabel="Session Step",
-            ylabel="Sessions Count",
-            title="Sessions Count by Session Step",
+            xlabel='Session Step',
+            ylabel='Sessions Count',
+            title='Sessions Count by Session Step',
             figsize=figsize,
         )
 
     def plot_user_sessions_mean_average_precision(self, figsize=(20, 6)):
         dpl.describe_num_var_array(
             self.sessions.mean_average_precision,
-            "Mean Average Precision by Session Distribution",
+            'Mean Average Precision by Session Distribution',
             figsize=figsize,
         )
 
     def plot_user_sessions_mean_reciprocal_rank(self, figsize=(20, 6)):
         dpl.describe_num_var_array(
             self.sessions.mean_reciprocal_rank,
-            "Mean Reciprocal Rank by Session Distribution",
+            'Mean Reciprocal Rank by Session Distribution',
             figsize=figsize,
         )
 
     def plot_user_session_steps_recall_dist(self, figsize=(20, 6)):
         dpl.describe_num_var_array(
             self.sessions.recall,
-            "Mean Recall by Session Step Distribution",
+            'Mean Recall by Session Step Distribution',
             figsize=figsize,
         )
 
@@ -185,7 +189,7 @@ class SessionsPlotter:
         figsize=(20, 6)
     ):
         if len(self.sessions) == 0:
-            logging.info("Not found sessions")
+            logging.info('Not found sessions')
             return None
 
         self.metrics(item_ids)
@@ -195,6 +199,11 @@ class SessionsPlotter:
         self.bar_plot_sessions_by_step(figsize=figsize)
 
         self.plot_n_rated_items_by_session_dist(figsize=figsize)
+
+        self.plot_n_rated_items_by_session_dist(
+            figsize=figsize,
+            min_found_relevant_items = 30
+        )
 
         self.plot_n_found_relevant_items_by_session_step_dist(figsize=figsize)
 
